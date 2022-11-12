c22de86de7630331b25033e9fa617c1ed8440828
victoria <hofty95@gmail.com>
2022-11-12 13:02:31 -0300

    add movies to movies.js

---
+++ movies.js
@@ -0,0 +1,47 @@
+module.exports = [
+    {
+        id : 1,
+        title : "John Wick",
+        rating : 8,
+        awards : 4,
+        length : 190,
+        price : "$450",
+        genre : "Acción"
+    },
+    {
+        id : 2,
+        title : "The Nightmare Before Chritsmas",
+        rating : 10,
+        awards : 100,
+        length : 90,
+        price : "$300",
+        genre : "Animación"
+    },
+    {
+        id : 3,
+        title : "Jumanji",
+        rating : 7,
+        awards : 2,
+        length : 120,
+        price : "$250",
+        genre : "Aventuras"
+    },
+    {
+        id : 4,
+        title : "Ace Ventura",
+        rating : 7,
+        awards : 4,
+        length : 130,
+        price : "$300",
+        genre : "Comedia"
+    },
+    {
+        id : 5,
+        title : "Donny Darko",
+        rating : 8,
+        awards : 5,
+        length : 145,
+        price : "$400",
+        genre : "Suspenso"
+    }
+]
