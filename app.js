7e60a9928c2aecd8a14f5867dabdeb178480762b
victoria <hofty95@gmail.com>
2022-11-12 13:57:02 -0300

    add function searchMovies to app.js

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const movies = require ("./movies")
 const moviesDH = {
     listMovies : function (){
         console.log(movies)
+    },
+    searchMovies : function(id){
+        let movieFound;
+        movieFound = movies.filter(function (movie){
+            return movie.id === id
+        })
+        return movieFound.length ? movieFound : "no se encuentra la pelicula requerida"
+
     }
 }
 
-return moviesDH.listMovies()
+console.log(moviesDH.searchMovies(2))
